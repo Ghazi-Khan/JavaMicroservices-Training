61cb0bc8549e0461e724723d0133761f445f8cff
Ghazi Khan <mailme.gkhan@gmail.com>
2022-07-12 21:40:31 +0530

    Created StreamApiDemo class - intermediate code

---
+++ Java-Beyond-Basic/JavaBeyondBasicCodeBase/src/com/infy/streamapi/StreamApiDemo.java
@@ -0,0 +1,11 @@
+package com.infy.streamapi;
+
+public class StreamApiDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test..");
+
+	}
+
+}
