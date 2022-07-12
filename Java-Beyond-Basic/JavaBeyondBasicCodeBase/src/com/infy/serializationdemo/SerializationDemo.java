e2fa81b80f250a7abb9e4b61a7a5212bd6151232
Ghazi Khan <mailme.gkhan@gmail.com>
2022-07-12 16:26:04 +0530

    Created SerializationDemo class - intermediate code

---
+++ Java-Beyond-Basic/JavaBeyondBasicCodeBase/src/com/infy/serializationdemo/SerializationDemo.java
@@ -0,0 +1,11 @@
+package com.infy.serializationdemo;
+
+public class SerializationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+
+	}
+
+}
